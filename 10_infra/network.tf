926ca4649a3025bbda3f826e96249ccb13495bbf
liu <echoliuxiang@yahoo.co.jp>
2025-02-24 15:26:41 +0900

    フォマード修正

--- 10_infra/network.tf
+++ 10_infra/network.tf
@@ -2,10 +2,10 @@
 # VPC
 # ---------------------------------------------
 resource "aws_vpc" "vpc" {
-  cidr_block                       = "192.168.0.0/20"
+  cidr_block= "192.168.0.0/20"
   instance_tenancy                 = "default"
   enable_dns_support               = true
-  enable_dns_hostnames             = true
+  enable_dns_hostnames= true
   assign_generated_ipv6_cidr_block = false
 
   tags = {
@@ -47,9 +47,9 @@ resource "aws_subnet" "public_subnet_1c" {
 }
 
 resource "aws_subnet" "private_subnet_1a" {
-  vpc_id                  = aws_vpc.vpc.id
+  vpc_id= aws_vpc.vpc.id
   availability_zone       = "ap-northeast-1a"
-  cidr_block              = "192.168.3.0/24"
+  cidr_block= "192.168.3.0/24"
   map_public_ip_on_launch = false
 
   tags = {
